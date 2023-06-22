41a826a170ac5f99d14badbd7dd07807dfe4718b
IGOR CRUZ <igorcruz.it@outlook.com>
2023-06-21 21:06:49 -0300

    Editando o tailwindcss

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
 
     <header className=' bg-slate-300 '>
-      <h1 className=' text-center p-40 text-red-600'>Meu Gourmet Favorito</h1>
+      <h1 className=' text-center p-4 text-red-600'>Meu Gourmet Favorito</h1>
     </header>
 
 
