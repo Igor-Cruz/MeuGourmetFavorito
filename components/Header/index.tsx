f57195a2bd98ef7101daab822f0d2f4a3e88ba68
IGOR CRUZ <igorcruz.it@outlook.com>
2023-06-22 06:38:39 -0300

    Navbar finalizado

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
 
-    <header className=' bg-gray-300 '>
-      <h1 className=' text-center p-4 text-slate-800'>Meu Gourmet Favorito</h1>
+    <header className=' bg-gray-300  '>
+      <h1 className=' text-center p-4 text-pink-600'>Meu Gourmet Favorito</h1>
     </header>
 
 
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -2,22 +2,22 @@ import Link from "next/link"
 
 export default function Navbar() {
   return (
-    <nav className='bg-gray-300 text-slate-800 '>
-      <ul >
+    <nav className='bg-gray-300 text-slate-800'>
+      <ul className=" p-4 text-end">
 
-        <li className="hover:text-pink-700">
+        <li className="hover:text-pink-700  list-none inline px-2">
           <Link href='#Home'>Home</Link>
         </li>
 
-        <li className="hover:text-pink-700">
+        <li className="hover:text-pink-700  list-none inline  px-2">
           <Link href='#Pedido'>Pedido</Link>
         </li>
 
-        <li className="hover:text-pink-700">
+        <li className="hover:text-pink-700  list-none inline  px-2">
           <Link href='#Contato'>Contato</Link>
         </li>
 
-        <li className="hover:text-pink-700">
+        <li className="hover:text-pink-700  list-none inline  px-2">
           <a href="login.tsx" target="_blank">Login</a>
         </li>
 
